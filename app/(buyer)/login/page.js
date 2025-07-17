019b44057bf741858f8c133dc810ad448884318d
Arjun3748 <arjunshanmugam2023ece@sece.ac.in>
2025-07-17 23:56:32 +0530

    Changes

--- app/(buyer)/login/page.js
+++ app/(buyer)/login/page.js
@@ -44,50 +44,51 @@ export default function Login() {
   </form>
 </div>
   );
-}'use client';
-
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import './login.css'
-export default function Login() {
-  const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-   localStorage.setItem('Email', email);
-    localStorage.setItem('Password', password);
-
-    console.log('Email:', email);
-    console.log(' Password:', password);
-
-    setEmail('');
-    setPassword('');
- router.push('/viewblog');
-  };
-
-  return (
-    <div className="login-container">
-  <form onSubmit={handleLogin} className="login-form">
-    <h2> user Login</h2>
-    <input
-      type="email"
-      placeholder="Email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      required
-    />
-    <input
-      type="password"
-      placeholder="Password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      required
-    />
-    <button type="submit">Login</button>
-  </form>
-</div>
-  );
 }
+// 'use client';
+
+// import { useRouter } from 'next/navigation';
+// import { useState } from 'react';
+// import './login.css'
+// export default function Login() {
+//   const router = useRouter();
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleLogin = (e) => {
+//     e.preventDefault();
+
+//    localStorage.setItem('Email', email);
+//     localStorage.setItem('Password', password);
+
+//     console.log('Email:', email);
+//     console.log(' Password:', password);
+
+//     setEmail('');
+//     setPassword('');
+//  router.push('/viewblog');
+//   };
+
+//   return (
+//     <div className="login-container">
+//   <form onSubmit={handleLogin} className="login-form">
+//     <h2> user Login</h2>
+//     <input
+//       type="email"
+//       placeholder="Email"
+//       value={email}
+//       onChange={(e) => setEmail(e.target.value)}
+//       required
+//     />
+//     <input
+//       type="password"
+//       placeholder="Password"
+//       value={password}
+//       onChange={(e) => setPassword(e.target.value)}
+//       required
+//     />
+//     <button type="submit">Login</button>
+//   </form>
+// </div>
+//   );
+// }
---
+++ app/(buyer)/viewblog/page.js
@@ -0,0 +1,7 @@
+
+const ViewBlog = () => {
+  return (
+    <div>This is vew blog page</div>
+  )
+}
+export default ViewBlog;
